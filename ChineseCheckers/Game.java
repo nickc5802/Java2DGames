b1549d1be5552322cbd83b0dbc60eb6eaa09eaf3
Nick <nickc5802@gmail.com>
2019-10-14 23:05:25 -0400

    Board positions are now drawn

--- ChineseCheckers/Game.java
+++ ChineseCheckers/Game.java
@@ -11,23 +11,30 @@ public class Game extends JPanel {
     public Game(int teams) {
         super();
         this.teams = teams;
-        board = new Piece[17][];
+        board = new Piece[17][1];
         for (int i = 0; i < 4; i++) {
             board[i] = new Piece[i+1];
-            board[17-i-1] = new Piece[i];
+            board[17-i-1] = new Piece[i+1];
         }
-        for (int i = 12; i > 8; i--) {
-            board[13-i+4] = new Piece[i];
-            board[i] = new Piece[i];
+        for (int i = 4; i < 8; i++) {
+            board[i] = new Piece[13-(i-4)];
+            board[12-(i-4)] = new Piece[13-(i-4)];
         }
         board[8] = new Piece[9];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = null;
+            }
+        }
         repaint();
     }
 
     @Override
     public void paint(Graphics g) {
+        //Background
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, 500, 500);
+        //hexagon
         g.setColor(Color.GRAY);
         Polygon polygon = new Polygon();
         for (int i = 0; i < 6; i++) {
@@ -36,12 +43,11 @@ public class Game extends JPanel {
             polygon.addPoint(xval, yval);
         }
         g.fillPolygon(polygon);
-
+        //points of star
         Color[] colors = new Color[] {
                 Color.RED, Color.BLUE, Color.CYAN, Color.YELLOW, Color.PINK, Color.GREEN
             };
         Polygon tri;
-        //(int)(250+250*Math.sin(Math.PI/3))
         for (int i = 0; i < 6; i++) {
             g.setColor(colors[i]);
             tri = new Polygon();
@@ -53,6 +59,17 @@ public class Game extends JPanel {
                 tri.addPoint(polygon.xpoints[i+1], polygon.ypoints[i+1]);
             }
             g.fillPolygon(tri);
+        }
+        //Board spots
+        g.setColor(Color.LIGHT_GRAY);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i].length%2==1) {
+                    g.fillOval(250 + (j-(board[i].length-1)/2)*28-8, 250-216+8+(int)(24.9*i), 16, 16);
+                } else {
+                    g.fillOval(250 + (j-(board[i].length-1)/2)*28-8-14, 250-216+8+(int)(24.9*i), 16, 16);
+                }
+            }
         }
     }
 }
